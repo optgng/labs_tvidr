ce6f21f2333102f3ea42b7b98f8b04903b1373da
Daniil D. <dandov9@gmail.com>
2024-10-15 23:57:37 +0300

    fix errors

--- src/convert/split_to_ycrcb_grey.py
+++ src/convert/split_to_ycrcb_grey.py
@@ -1,14 +1,22 @@
 from PIL import Image
 from core.rgb_to_ycrcb import calc_rgb_to_ycrcb
+from utils.logger import get_logger
+
+
+logger = get_logger(__name__)
 
 
 def split_to_ycrcb_grey(image: Image) -> (Image, Image, Image):
     width, height = image.size
 
+    logger.debug(f"Image size: {width}x{height}")
+    logger.info("Creating YCrCb images...")
+
     y_image = Image.new('L', (width, height))
     cr_image = Image.new('L', (width, height))
     cb_image = Image.new('L', (width, height))
 
+    logger.info("Converting pixels...")
     for x in range(width):
         for y in range(height):
             r, g, b = image.getpixel((x, y))
--- src/main.py
+++ src/main.py
@@ -1,7 +1,7 @@
 from utils.logger import get_logger
 from services.load_image import load_image
 from services.save_image import save_image
-from convert.split_to_ycrcb_coloured import split_to_ycrcb_coloured
+from convert.split_to_ycrcb_grey import split_to_ycrcb_grey
 
 logger = get_logger(__name__)
 
@@ -11,11 +11,12 @@ def main():
     logger.debug(f"Image path: {image_path}")
     bmp_image = load_image(image_path)
 
-    y_image, cr_image, cb_image = split_to_ycrcb_coloured(bmp_image)
+    y_image, cr_image, cb_image = split_to_ycrcb_grey(bmp_image)
 
     save_image(y_image, 'data/y.bmp')
     save_image(cr_image, 'data/cr.bmp')
     save_image(cb_image, 'data/cb.bmp')
+
 
 if __name__ == '__main__':
     main()
